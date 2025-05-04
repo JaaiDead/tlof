c693d8d7d7351fd853193c8b50933a6659fafdfb
JaaiDead <arcticquests.dev@gmail.com>
2025-05-04 23:32:44 +0530

    Start 1

---
+++ src/main/java/org/arcticquests/dev/item/ModItems.java
@@ -0,0 +1,19 @@
+package org.arcticquests.dev.item;
+
+import net.minecraft.world.item.Item;
+import net.minecraftforge.eventbus.api.IEventBus;
+import net.minecraftforge.registries.DeferredRegister;
+import net.minecraftforge.registries.ForgeRegistries;
+import net.minecraftforge.registries.RegistryObject;
+import org.arcticquests.dev.tlof;
+
+public class ModItems {
+    public static final DeferredRegister<Item> ITEMS = DeferredRegister.create(ForgeRegistries.ITEMS, tlof.MOD_ID);
+
+
+    public static final RegistryObject<Item> GUM = ITEMS.register("gum",() -> new Item(new Item.Properties()));
+
+    public static void register(IEventBus eventBus){
+        ITEMS.register(eventBus);
+    }
+}
